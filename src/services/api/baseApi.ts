e6b4dafa99e789229787c440653c722f3c579607
aks11s <aksis2003@mail.ru>
2024-12-20 22:39:22 +0300

    audius full API prefix in baseApi

--- src/services/api/baseApi.ts
+++ src/services/api/baseApi.ts
@@ -1,14 +1,15 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 import type {BaseQueryFn, FetchArgs, FetchBaseQueryError} from '@reduxjs/toolkit/query/react';
 import {getAudiusHost} from './audiusHost';
+ 
+export type AudiusQueryOptions = {
+  full?: boolean;
+};
 
-const dynamicBaseQuery: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError> = async (
-  args,
-  api,
-  extraOptions,
-) => {
+const dynamicBaseQuery: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError, AudiusQueryOptions> = async (args, api, extraOptions) => {
   const host = await getAudiusHost();
-  const rawBaseQuery = fetchBaseQuery({baseUrl: `${host}/v1`});
+  const prefix = extraOptions?.full ? '/v1/full' : '/v1';
+  const rawBaseQuery = fetchBaseQuery({baseUrl: `${host}${prefix}`});
   return rawBaseQuery(args, api, extraOptions);
 };
 
